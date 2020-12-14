f1c58ba08bc3fd85ea12fc4fd4a279cfe098da44
Trong Tan <trongnv4@onemount.com>
2020-12-14 16:49:51 +0700

    init app

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,50 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+
+const listCatName = ["Trong", "Trong 2"];
 
 export default function TabOneScreen() {
+  const [textValue, setTextValue] = useState("");
+  const [listCatNames, setListCatNames] = useState(listCatName);
+  const isOnchangeText = (text: string) => {
+    setTextValue(text);
+  };
+  const addCatToList = () => {
+    if (textValue) {
+      setListCatNames((currentValue) => [...currentValue, textValue]);
+      setTextValue("");
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <FlatList
+        data={listCatNames}
+        renderItem={({ item }) => <Text key={item}>{item}</Text>}
+      />
+      <Text style={styles.title}></Text>
+      <View style={{ padding: 10, backgroundColor: "Yellow" }}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Input your Cat name"
+          onChangeText={(text) => isOnchangeText(text)}
+          value={textValue}
+        />
+      </View>
+      <Text>{textValue}</Text>
+      <View>
+        <Button title="Add to list Cats!" onPress={addCatToList} />
+      </View>
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
   );
 }
@@ -17,16 +52,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
